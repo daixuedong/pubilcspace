83c35deb5a7d98db60b6caa98f69a594c9b419b6
daixuedong <daixuedong06@outlook.com>
2023-04-01 17:43:45 +0800

    hero

--- python_choujiang/app.py
+++ python_choujiang/app.py
@@ -1,22 +1,19 @@
-from flask import Flask,render_template,make_response
+from flask import Flask,render_template
 import random
 
 
 app=Flask(__name__)
 
 hero=['张三','李四','王五','赵六','钱七']
-random_hero=random.choice(hero)
 
 @app.route('/')
 def index():
-    response=make_response(render_template('index.html',hero=hero))
-    response.headers['Cache-Control']='max-age=300'
-    return response
+    return render_template('index.html',hero=hero)
 
 @app.route('/draw')
 def draw():
-    h=random.choice(hero)
-    return render_template('index.html',h=random_hero)
+    random_hero=random.choice(hero)
+    return render_template('index.html',hero=hero,h=random_hero)
 
 
 if __name__ == '__main__':
