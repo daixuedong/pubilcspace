e31fa38be65e2b9913cb87b619f6891c3e44126f
daixuedong <daixuedong06@outlook.com>
2023-04-01 13:58:45 +0800

    抽奖程序

--- python_flask/app.py
+++ python_flask/app.py
@@ -1,13 +1,22 @@
-# 导入flask
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+import random
 
 app=Flask(__name__)
 
-@app.route('/index')
+# 抽奖参与者
+participants=['张三','李四','王五','赵六','钱七']
+
+@app.route('/')
 def index():
-    return "dai"
+    return render_template('index.html')
+
+@app.route('/draw',methods=['POST'])
+def draw():
+    # 随机选取一人
+    winner=random.choice(participants)
+    # 返回中奖者名字
+    return render_template('draw.html',winner=winner)
 
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
